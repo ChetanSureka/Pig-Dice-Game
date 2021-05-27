d3a5d716665d7ca7a979eda0e6fa3f04d315da9f
ChetanSureka <69005536+ChetanSureka@users.noreply.github.com>
2021-05-27 13:01:09 +0530

    Update main.py
    A basic working version of dice game (Single player).

--- main.py
+++ main.py
@@ -11,19 +11,25 @@ def hold(current_scr, current_player):
     total_scr = current_scr
     global PLAYER_SCORE, COMPUTER_SCORE
     if current_player == "player":
-        print("Your total score is: ", total_scr)
-        if total_scr >= 100:
+        print("\n\t\t\tYour score: ", total_scr)
+        if total_scr >= 20:
             print("YOU WIN !!!\n\nGame Over")
         else:
             PLAYER_SCORE = total_scr
             print("The die is passed on to the computer.")
+            set_current_player_to("computer")
             
-    else:
-        print("The die is passed on to the next player.")
-    return total_scr
+    elif current_player == "computer":
+        print("\n\n\t\t\tComputer score: ", total_scr)
+        if total_scr >= 20:
+            print("I WON !!!\n\nGame Over")
+        else:
+            COMPUTER_SCORE = total_scr
+            print("The die is passed on to the next player.")
+            set_current_player_to("player")
 
 def gui():
-    print("Select any one option:\n1. Roll\n2. Hold")
+    print("\nSelect any one option:\n1. Roll\n2. Hold")
     player_decision = int(input("Enter your choice: "))
     return player_decision
 
@@ -31,36 +37,56 @@ def comp_dec_hold():
     decision = random.randint(1,2)
     return decision
 
+def set_current_player_to(player):
+    global COMPUTER_SCORE, PLAYER_SCORE
+    com_scr = COMPUTER_SCORE
+    player_scr = PLAYER_SCORE
+    if player == "computer":
+        computer_logic(com_scr)
+    elif player == "player":
+        player_logic(player_scr)
+
 def player_logic(previous_scr):
+    global PLAYER_SCORE
+    print("[Player's turn]\n")
     die_val = roll()
     print("Value in dice: ", die_val)
     if die_val == 1:
         current_scr = 0
+        PLAYER_SCORE = current_scr
+        print("Oops! looks like a wrong decision!\nYour total score now is: ", current_scr)
+        set_current_player_to("computer")
     elif die_val != 1 and die_val <= 6:
         current_scr = previous_scr + die_val
         player_decision = gui()
-        if player_decision == 1:
+        if player_decision == 1:    # Roll the dice once more
             previous_scr = current_scr
             player_logic(previous_scr)
-        elif player_decision == 2:
+        elif player_decision == 2:  # Hold the dice, and keep the score.
             hold(current_scr, "player")
 
 def computer_logic(previous_scr):
+    global COMPUTER_SCORE
+    print("[Computer's turn]\n")
     die_val = roll()
-    print("Value in Dice: ", die_val)
+    print("\nValue in Dice: ", die_val)
     if die_val == 1:
         current_scr = 0
+        COMPUTER_SCORE = current_scr
+        print("Oh no! I got 1!\nComputer score: ", current_scr)
+        set_current_player_to("player")
     elif die_val != 1 and die_val <= 6:
         current_scr = previous_scr + die_val
         decision = comp_dec_hold()
         if decision == 1:
-            print("I choose to roll the dice.")
+            print("\nI choose to roll the dice.")
             previous_scr = current_scr
             computer_logic(previous_scr)
         elif decision == 2:
-            print("I choose to hold the dice.")
-            hold(current_scr)
+            print("\nI choose to hold the dice.")
+            hold(current_scr,"computer")
 
+print("\n\n\t\t\tWelcome to the pig game\n")
 player_logic(0)
 
 
